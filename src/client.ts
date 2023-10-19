f2a6da58344f28b4ec86340f1467fcdc6ca71744
Belkross <belkross.pro@gmail.com>
2023-10-19 16:37:36 +0200

    feat: remove unfinished webhooks content

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,6 @@
 import { RequestSender } from './request-sender.js';
 import { AccountResource } from './resources/account.resource.js';
 import { MessagingResource } from './resources/messaging.resource.js';
-import { WebhookResource } from './resources/webhook.resource.js';
 import { ClientState } from './state.js';
 import { ClientInstantiationOptions } from './types/client.js';
 
@@ -12,7 +11,6 @@ export class UnipileClient {
   //resources
   public account = new AccountResource(this);
   public messaging = new MessagingResource(this);
-  public webhook = new WebhookResource(this);
 
   constructor(baseUrl: string, token: string, options?: ClientInstantiationOptions) {
     this.state = new ClientState(baseUrl, token, options ?? {});
