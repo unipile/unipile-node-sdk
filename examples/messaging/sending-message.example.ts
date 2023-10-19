b95564d7d8bdb8ac479cc22465c71d1a55c7c080
Belkross <belkross.pro@gmail.com>
2023-10-19 18:18:40 +0200

    docs: remove examples

--- examples/messaging/sending-message.example.ts
+++
@@ -1,9 +0,0 @@
-async function main() {
-  try {
-    // sending message scenario
-    // get all chats of a certain provider with a limit
-    // send message
-  } catch (error) {
-    console.error('App error:', error);
-  }
-}
